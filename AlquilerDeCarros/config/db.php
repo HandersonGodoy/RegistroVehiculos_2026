276ad8d21ea92eee8fae74942fcd9527c35bb89f
HandersonGodoy <hfblanco@uniboyaca.edu.co>
2026-04-10 19:55:27 -0500

    Arreglo V2 Base de Datos, nuevos cambios al proyecto

--- AlquilerDeCarros/config/db.php
+++ AlquilerDeCarros/config/db.php
@@ -1,7 +1,7 @@
 <?php
 class DB{
     public static function conectar(){
-        $conexion = new mysqli("localhost","root","","alquiler_vehiculos_db");
+        $conexion = new mysqli("localhost","root","","alquiler_vehiculos_db", 3307);
 
         if($conexion->connect_error){
             die("Error de conexión: " . $conexion->connect_error);
