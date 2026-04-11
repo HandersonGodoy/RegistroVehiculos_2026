626808e0254f0956045f9578860134b274f09a08
HandersonGodoy <hfblanco@uniboyaca.edu.co>
2026-04-10 20:20:42 -0500

    Cambios puerto Base de Datos, correción de Index, nuevos cambios al proyecto

--- AlquilerDeCarros/config/db.php
+++ AlquilerDeCarros/config/db.php
@@ -1,7 +1,7 @@
 <?php
 class DB{
     public static function conectar(){
-        $conexion = new mysqli("localhost","root","","alquiler_vehiculos_db", 3307);
+        $conexion = new mysqli("localhost","root","","alquiler_vehiculos_db",3306);
 
         if($conexion->connect_error){
             die("Error de conexión: " . $conexion->connect_error);
